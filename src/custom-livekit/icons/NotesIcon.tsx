3df926d090cec9a19c84e23ebcedc76983340c40
최수빈 <lightsaber2929@gmail.com>
2024-12-01 13:53:54 +0900

    ✨feat: 라이브킷 아이콘 추가

---
+++ src/custom-livekit/icons/NotesIcon.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+
+export function NotesIcon() {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M8 2v4h8V2M3 6v14a2 2 0 002 2h14a2 2 0 002-2V6M8 13h8M8 17h5"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+} 
