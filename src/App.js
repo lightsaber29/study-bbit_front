5caf67f78c59dae16f319f483ef0a8c8ca45839f
최수빈 <lightsaber2929@gmail.com>
2024-12-02 15:32:47 +0900

    ♻️refactor: 로딩 화면 개선

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useEffect } from 'react';
 import { Route, Routes } from "react-router-dom";
+import { useDispatch } from 'react-redux';
+import axios from 'api/axios';
+import { setMember } from 'store/memberSlice';
 import routes from './routes';
+import Loading from './components/Loading';
 
 function App() {
+
+
   return (
     <div className="max-w-screen-lg container mx-auto p-6">
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={<Loading />}>
         <Routes>
           {routes.map((route) => (
             <Route
