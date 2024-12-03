d654e11418a0e105fccac4744d4d6eedb71a5ef7
최수빈 <lightsaber2929@gmail.com>
2024-12-03 15:00:01 +0900

    🐛fix: 프록시 파일 수정

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -6,6 +6,7 @@ module.exports = function(app) {
   app.use(
     '/api/express',
     createProxyMiddleware({
+      // target: 'http://localhost:6081', // Node.js 서버로 프록시
       target: 'https://node.studybbit.site', // Node.js 서버로 프록시
       changeOrigin: true,
       logLevel: 'debug'
@@ -15,7 +16,7 @@ module.exports = function(app) {
   app.use(
     '/api', // proxy가 필요한 path prameter를 입력합니다.
     createProxyMiddleware({
-      // target: 'http://localhost:6081', //타겟이 되는 api url를 입력합니다.
+      // target: 'http://localhost:8080', //타겟이 되는 api url를 입력합니다.
       target: 'https://studybbit.store:8080', //타겟이 되는 api url를 입력합니다.
       changeOrigin: true, // 대상 서버 구성에 따라 호스트 헤더가 변경되도록 설정하는 부분입니다.
       logLevel: 'debug'
